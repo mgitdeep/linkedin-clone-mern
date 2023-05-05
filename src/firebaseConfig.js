d4acb0826caa4cbf9a3ab4775982f8e4209d2e1e
Mondeep Kalita <epicdeep09@gmail.com>
2023-05-05 19:24:22 +0530

    Initial set up with Vite

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3__266Fbck-BDYjOL0-zmaaFNWtS8gcc",
+  authDomain: "linkedin-clone-ba178.firebaseapp.com",
+  projectId: "linkedin-clone-ba178",
+  storageBucket: "linkedin-clone-ba178.appspot.com",
+  messagingSenderId: "1019586059556",
+  appId: "1:1019586059556:web:0529c94dc8f5f8b8d667b1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
